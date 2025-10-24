5080e0d80c870dc6cecceae0bd41489bac64a187
Gustavo Farias <gustavojosefariasdesouza@gmail.com>
2025-10-24 09:25:12 -0300

    feature: add refresh token

--- gateway-service/src/main/java/com/capbank/gateway_service/infra/filter/JwtAuthenticationFilter.java
+++ gateway-service/src/main/java/com/capbank/gateway_service/infra/filter/JwtAuthenticationFilter.java
@@ -61,7 +61,8 @@ public class JwtAuthenticationFilter implements WebFilter {
         return path.startsWith("/api/auth")
                 || path.startsWith("/api/user")
                 || path.startsWith("/api/gateway/user-registered")
-                || path.startsWith("/actuator");
+                || path.startsWith("/actuator")
+                || path.startsWith("/api/auth/refresh");
     }
 
     private String safeHash(String value) {

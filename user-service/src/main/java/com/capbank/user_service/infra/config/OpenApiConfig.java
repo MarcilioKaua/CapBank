f5c186c6b2b370b85863628e1955024db6e61414
Marcilio Silva <marciliokaua12@gmail.com>
2025-10-27 17:03:13 -0300

    feat: add user service swagger

---
+++ user-service/src/main/java/com/capbank/user_service/infra/config/OpenApiConfig.java
@@ -0,0 +1,24 @@
+package com.capbank.user_service.infra.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("User Service API")
+                        .version("1.0.0")
+                        .description("API para registro e consulta de usuários")
+                        .license(new License()
+                                .name("MIT License")
+                                .url("https://opensource.org/licenses/MIT"))
+                );
+    }
+}
